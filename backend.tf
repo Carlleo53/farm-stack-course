c8e09343c9f4b92087ec1f25902903f628b26729
Carlleo53 <charlesidoko53@gmail.com>
2025-07-17 13:49:13 +0100

    rearranged repo

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "carl-bestate-bucket"
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
-    use_lockfile   = true
+    dynamodb_table = "bestatetf-locks"
   }
 }
