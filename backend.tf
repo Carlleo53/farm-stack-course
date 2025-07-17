6bc667b9016b6902dde494aea960bb813a0cfe76
Carlleo53 <charlesidoko53@gmail.com>
2025-07-17 10:42:51 +0100

    modified

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "carl-bestate-bucket"
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "bestatetf-locks"
+    use_lockfile   = true
   }
 }
